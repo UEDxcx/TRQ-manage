b2ff834038ae947d5f11a51b045daa69f65bbf0b
maruize123 <436056164@qq.com>
2017-12-05 17:12:53 +0800

    commit log

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css'; 
+import axios from 'axios';
+
+Vue.use(iView);
 
 Vue.config.productionTip = false
-
+// ABC
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
